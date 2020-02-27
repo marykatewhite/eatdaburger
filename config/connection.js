f7833b4a2a8c4a3b6539b5c8382012dc17cd3dae
MaryEtak <marykatewhite@gmail.com>
2020-02-27 18:00:20 -0500

    lets do it again

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const util = require("util");
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
   const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "qr93kphy399nc3wm",
     port: 3306,
-    password: "Fr33dum!",
+    password: "iqj1h94vk0fott0c",
     database: "burgers_db"
   })
-};
+// };
 
 connection.connect();
 connection.query = util.promisify(connection.query);
