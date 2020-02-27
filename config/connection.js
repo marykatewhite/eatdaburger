a443dcac15054c243644c1b159663df19a4faada
MaryEtak <marykatewhite@gmail.com>
2020-02-27 18:05:39 -0500

    connection try

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const util = require("util");
 var mysql = require("mysql");
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   const connection = mysql.createConnection({
-    host: "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "qr93kphy399nc3wm",
+    host: "localhost",
+    user: "root",
     port: 3306,
-    password: "iqj1h94vk0fott0c",
+    password: "Fr33dum!",
     database: "burgers_db"
   })
-// };
+};
 
 connection.connect();
 connection.query = util.promisify(connection.query);
