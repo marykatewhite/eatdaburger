35d05446c4cd2d86564ca801c7d87d9d65605e0c
MaryEtak <marykatewhite@gmail.com>
2020-02-27 16:38:47 -0500

    trying to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-const util = require ('util')
+const util = require("util");
 var mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "Fr33dum!",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "Fr33dum!",
+    database: "burgers_db"
+  })
+};
+
 connection.connect();
-connection.query = util.promisify(connection.query)
+connection.query = util.promisify(connection.query);
 module.exports = connection;
