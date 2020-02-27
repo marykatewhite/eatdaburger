be843827f1508e75befc4e127b2a6357d42ec74f
MaryEtak <marykatewhite@gmail.com>
2020-02-27 18:10:26 -0500

    changing connection file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,13 @@
 const util = require("util");
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    port: 3306,
-    password: "Fr33dum!",
-    database: "burgers_db"
-  })
-};
+const connection = mysql.createConnection({
+  host: "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "qr93kphy399nc3wm",
+  port: 3306,
+  password: "iqj1h94vk0fott0c",
+  database: "burgers_db"
+});
 
 connection.connect();
 connection.query = util.promisify(connection.query);
